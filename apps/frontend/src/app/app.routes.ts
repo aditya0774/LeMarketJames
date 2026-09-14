c754f2cba6f01bd862524682ce20c9e73fdccf71
amaragrider <amara.grider@gmail.com>
2026-09-14 21:19:14 +0000

    feature: adding holdings routing for webpage

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
+import { HoldingsListComponent } from './features/holdings/holdings-list/holdings-list.component';
 
 export const routes: Routes = [
   { path: 'register', component: Register },
-  { path: 'login', component: Login }
+  { path: 'login', component: Login },
+  { path: 'holdings', component: HoldingsListComponent }
 ];
