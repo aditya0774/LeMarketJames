7b773f90b1f1d2e2ceff157b343a4df38cb59438
JohanDabare <johan@dabare.com>
2026-09-15 19:06:50 +0000

    feature: landing page added

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Register } from './features/auth/register/register';
 import { Login } from './features/auth/login/login';
+import { Home } from './features/home/home';
 
 export const routes: Routes = [
+  { path: '', component: Home },
   { path: 'register', component: Register },
   { path: 'login', component: Login }
 ];
